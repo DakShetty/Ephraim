e12c823766b0117d93d8c0ec048c46bccef2da91
DakShetty <daksh.shetty.1610@gmail.com>
2025-04-02 12:25:11 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+[4/2, 12:06] Ephraim T Philip: // script.js document.addEventListener("DOMContentLoaded", function () { const resumeButton = document.querySelector("button"); resumeButton.addEventListener("click", function () { alert("Not available currently"); }); });
+[4/2, 12:10] Ephraim T Philip: // script.js
+document.addEventListener("DOMContentLoaded", function () {
+    const resumeButton = document.querySelector("button");
+    resumeButton.addEventListener("click", function () {
+        alert("Not available currently");
+    });
+});
